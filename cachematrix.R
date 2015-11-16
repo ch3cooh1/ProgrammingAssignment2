8896b10c47e622fafcd497b6c6c766782fb3f93c
ch3cooh1 <balleman@huntoil.com>
2015-11-16 11:53:21 -0600

    Attempt 2
    Added comments and changed notation of variable m to invert

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,17 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The purpose of this file to create two functions.  The combined purpose of these is to solve for the inverse of
+## a matrix and store that solution as a cached value.  This will serve to conserve resources.
 
 ## This function will create a list that will feed a variable used in the following function.
 
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
+  invert <- NULL
   set <- function(y) {
     x <<- y
-    m <<- NULL
+    invert <<- NULL
   }
   get <- function() x
-  setinv <- function(solve) m <<- solve
-  getinv <- function() m
+  setinv <- function(solve) invert <<- solve
+  getinv <- function() invert
   list(set = set, get = get,
        setinv = setinv,
        getinv = getinv)
@@ -19,17 +19,19 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 
-## This function will generate the inverse of a matrix using values from makeCacheMatrix
+## This function will generate the inverse of a matrix using values from makeCacheMatrix.  It will first check to see
+## if the solution has already been generated.  If it has not been, then the solution will be found and stored in the
+## cache.  If the solution has been generated, then it will be retrieved from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-  m <- x$getinv()
-  if(!is.null(m)) {
+  invert <- x$getinv()
+  if(!is.null(invert)) {
     message("getting cached data")
-    return(m)
+    return(invert)
   }
   data <- x$get()
-  m <- solve(data, ...)
-  x$setinv(m)
-  m
+  invert <- solve(data, ...)
+  x$setinv(invert)
+  invert
 }
